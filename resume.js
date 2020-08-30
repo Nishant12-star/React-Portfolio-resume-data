ddc2091d35443868f7d05029823a8d1c7bd26650
Nishant12-star <66728845+Nishant12-star@users.noreply.github.com>
2020-08-30 22:32:19 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "name": "Nishant sharma",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"https://www.linkedin.com/in/nishant-sharma-695103151/",
+    "skypeid": "live:.cid.1ea5190125bb730b",
+    "roleDescription": "I like in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nishant-sharma-695103151/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Nishant12-star",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"live:.cid.1ea5190125bb730b",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"I am currently a Final  year student at Lovely Professional University and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Data science. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Nishant12-star",
+    "education":[
+      {
+        "UniversityName":"Lovely Professional University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
